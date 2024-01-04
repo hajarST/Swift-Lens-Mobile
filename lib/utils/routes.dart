2d67668e591bd68dddd1e9e2e0379d71476999fa
Hajar Sabt <hajarsabt@gmail.com>
2024-01-04 01:13:48 +0100

    update Scan

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,4 +1,6 @@
 class MyRoutes {
+  //MyRoutesPages
   static String uploadpage = '/upload';
   static String home = '/';
+
 }
--- lib/utils/scan.dart
+++ lib/utils/scan.dart
@@ -2,93 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class ScanPage extends StatefulWidget {
-  @override
-  _ScanPageState createState() => _ScanPageState();
-}
-
-class _ScanPageState extends State<ScanPage> {
-  static String apiEndpoint = 'localhost:8080/ocr';
-
-  Future<void> _read() async {
-    try {
-      // Capture image using camera or select existing image
-      String imagePath =
-          await captureImage(); // Implement your image capture logic here
-
-      String ocrResult = await performOCR(imagePath);
-
-      // Display or process OCR result as needed
-      print('OCR Result: $ocrResult');
-    } catch (e) {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('Error'),
-            content: Text('Failed to recognize text. Error: $e'),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: Text('OK'),
-              ),
-            ],
-          );
-        },
-      );
-    }
-
-    if (!mounted) return;
-    setState(() {});
-  }
-
-  Future<String> performOCR(String imagePath) async {
-    try {
-      var response = await http.post(
-        Uri.parse(apiEndpoint),
-        body: {'imagePath': imagePath},
-      );
-
-      if (response.statusCode == 200) {
-        Map<String, dynamic> data = json.decode(response.body);
-        // Assuming your API returns the OCR result as 'text'
-        return data['text'];
-      } else {
-        throw Exception(
-            'Failed to perform OCR. Status code: ${response.statusCode}');
-      }
-    } catch (e) {
-      throw Exception('Failed to perform OCR. Error: $e');
-    }
-  }
-
-  Future<String> captureImage() async {
-    // Implement your image capture logic here
-    // Example using image_picker:
-    // final picker = ImagePicker();
-    // var pickedFile = await picker.getImage(source: ImageSource.camera);
-    // return pickedFile.path;
-    return 'PATH_TO_YOUR_IMAGE';
-  }
-
-  void backpressed(BuildContext context) {
-    Navigator.pop(context);
-  }
+class ScanPage extends StatelessWidget {
+  static const String apiEndpoint = 'localhost:8080/ocr';
 
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async {
-        backpressed(context);
+        _onBackPressed(context);
         return false;
       },
       child: Card(
         color: Colors.grey.shade700,
         child: InkWell(
           onTap: () {
-            _read();
+            _read(context);
           },
           child: Container(
             decoration: BoxDecoration(
@@ -108,6 +36,63 @@ class _ScanPageState extends State<ScanPage> {
           ),
         ),
       ),
+    );
+  }
+
+  Future<void> _read(BuildContext context) async {
+    try {
+      String imagePath = await _captureImage();
+      String ocrResult = await _performOCR(imagePath);
+      print('OCR Result: $ocrResult');
+    } catch (e) {
+      _showErrorDialog(context, 'Failed to recognize text. Error: $e');
+    }
+  }
+
+  Future<String> _performOCR(String imagePath) async {
+    try {
+      var response = await http.post(
+        Uri.parse(apiEndpoint),
+        body: {'imagePath': imagePath},
+      );
+
+      if (response.statusCode == 200) {
+        Map<String, dynamic> data = json.decode(response.body);
+        return data['text'];
+      } else {
+        throw Exception('Failed to perform OCR. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Failed to perform OCR. Error: $e');
+    }
+  }
+
+  Future<String> _captureImage() async {
+    
+    return 'PATH_TO_YOUR_IMAGE';
+  }
+
+  void _onBackPressed(BuildContext context) {
+    Navigator.pop(context);
+  }
+
+  void _showErrorDialog(BuildContext context, String errorMessage) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Error'),
+          content: Text(errorMessage),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
